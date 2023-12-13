2172f00b2f5c407cefa4f057d08fb7f9175dc716
JackooDesu <45838705+JacKooDesu@users.noreply.github.com>
2023-12-14 01:31:45 +0800

    add invidious api settings
    - fix auto generated channel buggy thumbnail url
    - add .env setting template

--- src/bot/constants.rs
+++ src/bot/constants.rs
@@ -1,9 +1,12 @@
 pub const DISCORD_TOKEN_KEY: &str = "DISCORD_TOKEN";
 pub const YT_API_KEY: &str = "YT_API";
 pub const COMMAND_PREFIX_KEY: &str = "COMMAND_PREFIX";
+pub const REGION_KEY: &str = "REGION";
+pub const INVIDIOUS_INSTANCE_KEY: &str = "INVIDIOUS_INSTANCE";
 
 pub const REQUIRED_KEYS: &'static [&str] = &[DISCORD_TOKEN_KEY, YT_API_KEY];
-pub const OPTIONAL_KEYS: &'static [&str] = &[COMMAND_PREFIX_KEY];
+pub const OPTIONAL_KEYS: &'static [&str] =
+    &[COMMAND_PREFIX_KEY, REGION_KEY, INVIDIOUS_INSTANCE_KEY];
 
 pub const NUM_EMOJI: &'static [&str] = &["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣", "🔟"];
 
